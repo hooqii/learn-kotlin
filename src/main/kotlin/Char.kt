760806c5d2e5b6090a2199f2a58b7ff5686619d8
hooqii <hooqii.me@gmail.com>
2023-10-30 02:06:18 +0700

    Added Char.kt

---
+++ src/main/kotlin/Char.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var char = 'A'
+//  Increment A to B
+    char++
+//  Increment B to C
+    char++
+//  Decrement C to B
+    char--
+    println(char)
+//  It will print to console B
+}

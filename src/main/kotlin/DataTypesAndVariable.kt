623e790c6f14a57364657d5f7a96b71c32fdcd01
hooqii <hooqii.me@gmail.com>
2023-10-29 17:32:11 +0700

    Added DataTypes&Variable.kt

---
+++ src/main/kotlin/DataTypesAndVariable.kt
@@ -0,0 +1,16 @@
+/*
+Data Type On Kotlin :
+        - Numbers
+        - Booleans
+        - Characters
+        - String
+        - Arrays
+*/
+
+fun main() {
+    val number: Int = 1
+    val boolean: Boolean = true
+    val char: Char = 'A'
+    val string: String = "This Is A String"
+    println("$number $boolean $char $string")
+}
